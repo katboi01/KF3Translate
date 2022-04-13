3ea149bcb26b05acf97d94bbf16e4b7bd8ad920e
Katboi01 <katboiofficial@gmail.com>
2022-04-13 09:49:10 +0200

    Added deepl translation settings. Added runtime scenario swapping. Added a button to switch back into default mode.

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add *
-git commit -m "Fixed critical error with dropdowns"
+git commit -m "Added deepl translation settings. Added runtime scenario swapping. Added a button to switch back into default mode."
 git push origin master
